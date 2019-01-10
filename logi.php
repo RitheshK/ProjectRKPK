5a5e8ac2e4c7d1924a003970e36bbb8c5c8a9d73
Rithesh Kannan <rk4thewin@gmail.com>
2019-01-10 16:36:45 +0000

    navigation bars

---
+++ logi.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "127.0.0.1";
+$user = "RIthesh Kannan";
+$pass = "";
+$db = "project_db";
+$port = 3306;
+
+$connection = mysqli_connect($host,$user,$pass,$db,$port) or die(mysql_error());
+?>
